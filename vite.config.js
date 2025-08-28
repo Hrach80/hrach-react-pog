45a6df998b5720781c66636e38ba914198eed888
Hrach <hrachyavagarshakyan@gmail.com>
2025-08-28 15:05:48 +0400

    PWA nor

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa'; // Ավելացրեք այս տողը
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
